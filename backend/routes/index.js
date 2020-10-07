ec5916aad0a16f694e532f26d4da10cbdad8f4e5
steph <stephypadillaromero@gmail.com>
2020-10-08 00:13:00 +0200

    he vuelto a poner el index.js de routes como estaba

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,30 +1,10 @@
 'use strict'
 
-//Fichero que contiene las rutas de la API
-//Importamos y ejecutamos router de express
-const express = require('express')
-const userController = require('../controllers/user')
-
-//Router nos permite gestionar rutas de la API
-const router = express.Router()
-
-
-//Controlador de demo que contiene lo que se ejecutara
-//cuando hagamos la peticion
-
-//Peticiones HTTP
-router.get('/users', userController.getUsers),
-    router.get('/user/:userId', userController.getUser),
-    router.post('/register', userController.newUser),
-
-//Exportamos router para usar rutas en app.js
-    module.exports = router
-
-/*const express = require('express');
+const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router*/
+module.exports = router
